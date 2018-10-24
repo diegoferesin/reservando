5d06781d4127af4d8409e0d69c048bb796454fcd
Diego Feresin <diegoferesin@gmail.com>
2018-10-24 12:44:14 -0300

    Comencé con los test de Obtener Puntuación

--- src/js/tests.js
+++ src/js/tests.js
@@ -1,6 +1,6 @@
 var expect = chai.expect;
 
-describe('Reserva de horarios', function() {
+describe('Reserva de Horarios ⏰', function() {
   it('Cuando quiero reservar un horario, el array de horarios disminuye en 1 su cantidad', function() {
     var restaurant = new Restaurant(
       1,
@@ -19,7 +19,7 @@ describe('Reserva de horarios', function() {
   });
 });
 
-describe('Reserva de horarios', function() {
+describe('Reserva de Horarios ⏰', function() {
   it('Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.', function() {
     var restaurant = new Restaurant(
       2,
@@ -37,7 +37,7 @@ describe('Reserva de horarios', function() {
   });
 });
 
-describe('Reserva de horarios', function() {
+describe('Reserva de Horarios ⏰', function() {
   it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.', function() {
     var restaurant = new Restaurant(
       3,
@@ -47,6 +47,24 @@ describe('Reserva de horarios', function() {
       ['10', '11', '12'],
       'Imagen',
       [3, 4]
+    );
+    var cantidadPrevia = restaurant.horarios.length;
+    restaurant.reservarHorario('');
+
+    expect(restaurant.horarios.length).to.equal(cantidadPrevia);
+  });
+});
+
+describe('Obtener Puntuación ⭐️⭐⭐️️⭐️️⭐️️', function() {
+  it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.', function() {
+    var restaurant = new Restaurant(
+      4,
+      'Nombre',
+      'Rubro',
+      'Ubicación',
+      ['10', '11', '12'],
+      'Imagen',
+      [4, 5]
     );
     var cantidadPrevia = restaurant.horarios.length;
     restaurant.reservarHorario('');
