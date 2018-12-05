753d1f2070a6645b14984b13f262caad2e6dd785
Diego Feresin <diegoferesin@gmail.com>
2018-12-05 06:58:25 -0300

    Comencé con los test para la función obtenerPuntuación()

--- src/js/tests.js
+++ src/js/tests.js
@@ -55,8 +55,22 @@ describe('Reserva de Horarios ⏰', function() {
   });
 });
 
+// Restaurant.prototype.obtenerPuntuacion = function() {
+//   if (this.calificaciones.length === 0) {
+//     return 0;
+//   } else {
+//     var sumatoria = 0;
+//     for (var i = 0; i < this.calificaciones.length; i++) {
+//       sumatoria += this.calificaciones[i];
+//     }
+//     var promedio = sumatoria / this.calificaciones.length;
+//     return Math.round(promedio * 10) / 10;
+//   }
+// };
+// var Restaurant = function(id, nombre, rubro, ubicacion, horarios, imagen, calificaciones)
+
 describe('Obtener Puntuación ⭐️⭐⭐️️⭐️️⭐️️', function() {
-  it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.', function() {
+  it('Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.', function() {
     var restaurant = new Restaurant(
       4,
       'Nombre',
@@ -65,6 +79,23 @@ describe('Obtener Puntuación ⭐️⭐⭐️️⭐️️⭐️️', function() 
       ['10', '11', '12'],
       'Imagen',
       [4, 5]
+    );
+    var promedioCorrecto = (4 + 5) / 2;
+
+    expect().to.equal(promedioCorrecto);
+  });
+});
+
+describe('Obtener Puntuación ⭐️⭐⭐️️⭐️️⭐️️', function() {
+  it('Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.', function() {
+    var restaurant = new Restaurant(
+      5,
+      'Nombre',
+      'Rubro',
+      'Ubicación',
+      ['10', '11', '12'],
+      'Imagen',
+      [5, 6]
     );
     var cantidadPrevia = restaurant.horarios.length;
     restaurant.reservarHorario('');
